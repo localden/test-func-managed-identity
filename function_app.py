cbc370393ddef8b6eb4ff413598050cc7fa5f696
den (work) <53200638+localden@users.noreply.github.com>
2023-01-10 15:22:29 -0800

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -11,7 +11,7 @@ default_credential = DefaultAzureCredential()
 @app.route(route="test")
 def my_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    # Test2
+    # Test2w
     client = BlobServiceClient("https://teststoragemid.blob.core.windows.net", credential=default_credential)
     container_name = str(uuid.uuid4())
     container_client = client.create_container(container_name)
