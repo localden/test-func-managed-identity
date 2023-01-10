7659df8fb75fbeeb91f5eb7a7a7362e56baa9d56
den (work) <53200638+localden@users.noreply.github.com>
2023-01-10 13:12:18 -0800

    Update function code

--- function_app.py
+++ function_app.py
@@ -5,7 +5,9 @@ import azure.functions as func
 
 default_credential = DefaultAzureCredential()
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+@app.function_name(name="TestTrigger")
+@app.route(route="test")
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
     # Test
     client = BlobServiceClient(https://teststoragemid.blob.core.windows.net, credential=default_credential)
     container_name = str(uuid.uuid4())
