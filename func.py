dd486e96d6f8c4ebd8bcc33e795a4bfadd163e6b
den (work) <53200638+localden@users.noreply.github.com>
2023-01-10 12:14:53 -0800

    Update func.py

--- func.py
+++ func.py
@@ -3,6 +3,7 @@ from azure.storage.blob import BlobServiceClient
 
 default_credential = DefaultAzureCredential()
 
+# Test
 client = BlobServiceClient(https://teststoragemid.blob.core.windows.net, credential=default_credential)
 container_name = str(uuid.uuid4())
 container_client = client.create_container(container_name)
