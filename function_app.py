ae9480fd1e5b4e0a28dac517083e7b4cb3620476
den (work) <53200638+localden@users.noreply.github.com>
2023-01-10 13:49:29 -0800

    Update Azure data

--- function_app.py
+++ function_app.py
@@ -1,13 +1,15 @@
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
 import azure.functions as func
+import logging
 
 app = func.FunctionApp()
 default_credential = DefaultAzureCredential()
 
 @app.function_name(name="TestTrigger")
 @app.route(route="test")
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def my_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
     # Test
     client = BlobServiceClient(https://teststoragemid.blob.core.windows.net, credential=default_credential)
     container_name = str(uuid.uuid4())
